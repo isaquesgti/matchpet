a29ada03b5ea0800e5f83e498f28ac28f3ba1547
isaquesgti <isaque.ti@gmail.com>
2026-02-20 16:00:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,51 @@
 import { Toaster } from "@/components/ui/toaster";
-
 import { Toaster as Sonner } from "@/components/ui/sonner";
-
 import { TooltipProvider } from "@/components/ui/tooltip";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import { AuthProvider } from "@/hooks/useAuth";
-
 import Index from "./pages/Index";
-
 import Auth from "./pages/Auth";
-
 import Dashboard from "./pages/Dashboard";
-
 import PetForm from "./pages/PetForm";
-
 import Swipe from "./pages/Swipe";
-
 import Matches from "./pages/Matches";
-
 import Profile from "./pages/Profile";
-
 import Chat from "./pages/Chat";
-
 import PetDetails from "./pages/PetDetails";
-
 import Admin from "./pages/Admin";
-
 import NotFound from "./pages/NotFound";
-
-
+import TermsPage from "./components/Terms"; // Importando Termos
 
 const queryClient = new QueryClient();
-
-
-
 const App = () => (
-
   <QueryClientProvider client={queryClient}>
-
     <TooltipProvider>
-
       <AuthProvider>
-
         <Toaster />
-
         <Sonner />
-
         <BrowserRouter>
-
           <Routes>
-
             <Route path="/" element={<Index />} />
-
             <Route path="/auth" element={<Auth />} />
-
             <Route path="/dashboard" element={<Dashboard />} />
-
             <Route path="/pets/new" element={<PetForm />} />
-
             <Route path="/pets/:id" element={<PetForm />} />
-
             <Route path="/pets/:petId/details" element={<PetDetails />} />
-
             <Route path="/swipe" element={<Swipe />} />
-
             <Route path="/matches" element={<Matches />} />
-
             <Route path="/chat/:matchId" element={<Chat />} />
-
             <Route path="/profile" element={<Profile />} />
-
             <Route path="/admin" element={<Admin />} />
-
+            <Route path="/termos" element={<TermsPage />} />
+            
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-
             <Route path="*" element={<NotFound />} />
-
           </Routes>
-
         </BrowserRouter>
-
       </AuthProvider>
-
     </TooltipProvider>
-
   </QueryClientProvider>
-
 );
-
-
 
 export default App;
